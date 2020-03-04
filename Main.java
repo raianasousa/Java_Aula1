de863703c564bc598821542be973da53e01dd1bc
Raiana Sousa dos Santos <53270687+raianasousa@users.noreply.github.com>
2020-03-04 20:05:55 -0300

    Update Main.java
    Inserção de dados e classe Main

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ class Main{
   public static void main(String[] args){
    
     Media calculo = new Media();
-      calculo.nota1 = 10.0;
-      calculo.nota2 = 5.0;
-      calculo.nota3 = 8.0;
+      calculo.nota1 = idNota1;
+      calculo.nota2 = idNota2;
+      calculo.nota3 = idNota3;
     System.out.println(calculo.Calcularmedia());
   }
 }
