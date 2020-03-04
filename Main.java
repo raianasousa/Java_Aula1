22cd2a16ec1791da20e50346b0c711c0a35269e4
Raiana Sousa dos Santos <53270687+raianasousa@users.noreply.github.com>
2020-03-04 20:05:06 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main{
+  public static void main(String[] args){
+   
+    Media calculo = new Media();
+      calculo.nota1 = 10.0;
+      calculo.nota2 = 5.0;
+      calculo.nota3 = 8.0;
+    System.out.println(calculo.Calcularmedia());
+  }
+}
---
+++ Media.java
@@ -0,0 +1,10 @@
+class Media{
+
+  double nota1, nota2, nota3,media;
+
+  double Calcularmedia(){
+    media = (nota1 + nota2 + nota3)/3;
+    return media;
+  }
+
+}
